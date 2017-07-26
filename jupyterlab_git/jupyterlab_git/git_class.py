04feba7d7373fe686f2bac025f91439957f46d83
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-07-26 14:14:21 -0700

    Update git_class.py

--- jupyterlab_git/jupyterlab_git/git_class.py
+++ jupyterlab_git/jupyterlab_git/git_class.py
@@ -34,11 +34,47 @@ class Git:
         else:
             return {"code": p.returncode, "message": my_error.decode('utf-8')}
 
+    def log(self,top_repo_path):
+        p = Popen(["git", "log","--pretty=format:%an-%s-%ar"], stdout=PIPE, stderr=PIPE, cwd = top_repo_path)
+        my_output,my_error = p.communicate()
+        if(p.returncode==0):
+            result = []
+            line_array = my_output.decode('utf-8').splitlines()
+            for line in line_array:
+                linesplit = line.split('-')
+                result.append({'author':linesplit[0], 'commitMsg': linesplit[1],'commitDate':linesplit[2]})
+            return {"code": p.returncode, "commits":result}
+        else:
+            return {"code":p.returncode, "message":my_error.decode('utf-8')}
+
+    def diff(self,top_repo_path):
+        p = Popen(["git", "diff","--numstat"], stdout=PIPE, stderr=PIPE, cwd = top_repo_path)
+        my_output,my_error = p.communicate()
+        if(p.returncode==0):
+            result = []
+            line_array = my_output.decode('utf-8').splitlines()
+            for line in line_array:
+                linesplit = line.split()
+                result.append({'insertions':linesplit[0], 'deletions': linesplit[1],'filename':linesplit[2]})
+            return {"code": p.returncode, "result":result}
+        else:
+            return {"code":p.returncode, "message":my_error.decode('utf-8')}
+
+
     def showtoplevel(self, current_path):
         p = Popen(["git", "rev-parse", "--show-toplevel"], stdout=PIPE, stderr=PIPE, cwd = os.getcwd()+'/'+current_path)
         my_output, my_error = p.communicate()
         if(p.returncode==0):
             result ={"code": p.returncode, "top_repo_path": my_output.decode('utf-8').strip('\n')}
+            return result
+        else:
+            return {"code": p.returncode, "message": my_error.decode('utf-8')}
+
+    def showprefix(self, current_path):
+        p = Popen(["git", "rev-parse", "--show-prefix"], stdout=PIPE, stderr=PIPE, cwd = os.getcwd()+'/'+current_path)
+        my_output, my_error = p.communicate()
+        if(p.returncode==0):
+            result ={"code": p.returncode, "under_repo_path": my_output.decode('utf-8').strip('\n')}
             return result
         else:
             return {"code": p.returncode, "message": my_error.decode('utf-8')}
@@ -77,4 +113,18 @@ class Git:
         my_output = subprocess.check_output(["git", "commit", "-m", commit_msg], cwd = top_repo_path)
         print("Hi there! post extensions!!")
         return my_output
+
+    def pull(self, origin, master,top_repo_path):
+        my_output_1 = subprocess.check_output(["git", "fetch", "--all"], cwd = top_repo_path)
+        str1 = origin +"/"+master
+        my_output = subprocess.check_output(["git", "reset","--hard", str1], cwd = top_repo_path)
+        print("Hi there! post extensions!!")
+        return my_output
+
+    def push(self, origin, master,top_repo_path):
+        my_output = subprocess.check_output(["git", "push", origin,master], cwd = top_repo_path)
+        print("Hi there! post extensions!!")
+        return my_output
+
+
       
