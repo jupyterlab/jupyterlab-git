1bc65b43ca01cef6c8b2018d090468e02018ba73
JZ <zzhangjii@gmail.com>
2018-01-31 12:02:07 -0800

    change animation code for past commits, remove jQuery dependency

--- src/components/pastCommits.tsx
+++ src/components/pastCommits.tsx
@@ -15,7 +15,6 @@ import {
 } from './statusFiles'
 
 import '../../style/index.css';
-import $ = require('jquery');
 
 /**
  * The class name added to a git-plugin session item icon.
@@ -65,16 +64,22 @@ export class PastCommits extends React.Component<PastCommits.IProps, PastCommits
 
   show_left(){
     let pastcommitsContainer = ReactDOM.findDOMNode(this.refs.past_commits_container);
-      $(pastcommitsContainer).animate({scrollLeft: pastcommitsContainer.scrollLeft-200}, 320);
+      //$(pastcommitsContainer).animate({scrollLeft: pastcommitsContainer.scrollLeft-200}, 320);
+      smooth_scroll_to(pastcommitsContainer, pastcommitsContainer.scrollLeft - 200, 320);
       this.setState({show_right_arrow:true});
+      /*
       if(pastcommitsContainer.scrollLeft==0){
         this.setState({show_left_arrow:false});
       }
+      else{
+        this.setState({show_left_arrow:true});
+      }*/
   }
 
   show_right(){
     let pastcommitsContainer = ReactDOM.findDOMNode(this.refs.past_commits_container);
-      $(pastcommitsContainer).animate({scrollLeft: pastcommitsContainer.scrollLeft+200},320);
+      //$(pastcommitsContainer).animate({scrollLeft: pastcommitsContainer.scrollLeft+200},320);
+      smooth_scroll_to(pastcommitsContainer, pastcommitsContainer.scrollLeft + 200, 320);
       this.setState({show_left_arrow:true});
       if(pastcommitsContainer.scrollLeft>=pastcommitsContainer.scrollWidth-400){
         this.setState({show_right_arrow:false});
@@ -267,5 +272,82 @@ export class SinglePastCommitInfo extends React.Component<SinglePastCommitInfo.I
  *           <span className='jp-Git-icon-deletion'> {this.props.deletion_count}</span>
           <span className='jp-Git-icon-insertion'> {this.props.insertion_count}</span>
  */
+
+/**
+    This smooth_scroll_to Solution is from online resources, for temporary use, may change to react-motion in the future:
+
+    Smoothly scroll element to the given target (element.scrollLeft)
+    for the given duration
+
+    Returns a promise that's fulfilled when done, or rejected if
+    interrupted
+ */
+var smooth_scroll_to = function(element, target, duration) {
+  target = Math.round(target);
+  duration = Math.round(duration);
+  if (duration < 0) {
+      return Promise.reject("bad duration");
+  }
+  if (duration === 0) {
+      element.scrollLeft = target;
+      return Promise.resolve();
+  }
+
+  var start_time = Date.now();
+  var end_time = start_time + duration;
+
+  var start_top = element.scrollLeft;
+  var distance = target - start_top;
+
+  // based on http://en.wikipedia.org/wiki/Smoothstep
+  var smooth_step = function(start, end, point) {
+      if(point <= start) { return 0; }
+      if(point >= end) { return 1; }
+      var x = (point - start) / (end - start); // interpolation
+      return x*x*(3 - 2*x);
+  }
+
+  return new Promise(function(resolve, reject) {
+      // This is to keep track of where the element's scrollLeft is
+      // supposed to be, based on what we're doing
+      var previous_top = element.scrollLeft;
+
+      // This is like a think function from a game loop
+      var scroll_frame = function() {
+          if(element.scrollLeft != previous_top) {
+              reject("interrupted");
+              return;
+          }
+
+          // set the scrollLeft for this frame
+          var now = Date.now();
+          var point = smooth_step(start_time, end_time, now);
+          var frameTop = Math.round(start_top + (distance * point));
+          element.scrollLeft = frameTop;
+
+          // check if we're done!
+          if(now >= end_time) {
+              resolve();
+              return;
+          }
+
+          // If we were supposed to scroll but didn't, then we
+          // probably hit the limit, so consider it done; not
+          // interrupted.
+          if(element.scrollLeft === previous_top
+              && element.scrollLeft !== frameTop) {
+              resolve();
+              return;
+          }
+          previous_top = element.scrollLeft;
+
+          // schedule next frame for execution
+          setTimeout(scroll_frame, 0);
+      }
+
+      // boostrap the animation process
+      setTimeout(scroll_frame, 0);
+  });
+}
 
 
