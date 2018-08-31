3725112c3bf09c25e3165fbd1361354cb9c58bf0
jennalandy <jennalandy0@gmail.com>
2018-08-31 13:44:43 -0700

    removing comments

--- src/components/PathHeader.tsx
+++ src/components/PathHeader.tsx
@@ -2,10 +2,6 @@ import {
   repoStyle,
   repoPathStyle,
   repoRefreshStyle,
-  // repoIconStyle,
-  // arrowStyle,
-  // gitRepoPathContainerStyle,
-  // directoryStyle
 } from '../componentsStyle/PathHeaderStyle';
 
 import * as React from 'react';
@@ -40,14 +36,7 @@ export class PathHeader extends React.Component<
     let relativePath = this.props.currentFileBrowserPath.split('/');
     return (
       <div className={repoStyle}>
-        {/* <span className={repoIconStyle} /> */}
         <span className={repoPathStyle}>
-          {/* {relativePath.map(directory => (
-            <div key={directory} className={gitRepoPathContainerStyle}>
-              {relativePath[0] !== '' && <span className={arrowStyle} />}
-              <span className={directoryStyle}>{directory}</span>
-            </div>
-          ))} */}
           {relativePath[relativePath.length - 1]+" / "+this.props.currentBranch}
         </span>
         <button
