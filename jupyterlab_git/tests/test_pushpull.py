2b884c49ef3dd91b0f57709092b7ac1c61afbe2d
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-05-08 16:02:26 -0500

    test: Unit tests for git push/pull with auth on the backend

--- jupyterlab_git/tests/test_pushpull.py
+++ jupyterlab_git/tests/test_pushpull.py
@@ -33,6 +33,38 @@ def test_git_pull_fail(mock_subproc_popen):
     ])
     assert {'code': 1, 'message': 'Authentication failed'} == actual_response
 
+@patch('jupyterlab_git.git.git_auth_input_wrapper')
+@patch('os.environ', {'TEST': 'test'})
+def test_git_pull_with_auth_fail(mock_git_auth_input_wrapper):
+    # Given
+    process_mock = Mock()
+    attrs = {
+        'communicate.return_value': "remote: Invalid username or password.\r\nfatal: Authentication failed for 'repo_url'".encode('utf-8'),
+        'returncode': 1
+    }
+    process_mock.configure_mock(**attrs)
+    mock_git_auth_input_wrapper.return_value = process_mock
+
+    # When
+    auth = {
+        'username' : 'asdf', 
+        'password' : 'qwerty'
+    }
+    actual_response = Git(root_dir='/bin').pull('test_curr_path', auth)
+
+
+    # Then
+    mock_git_auth_input_wrapper.assert_has_calls([
+        call(
+            command = 'git pull --no-commit',
+            cwd='/bin/test_curr_path',
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '1'},
+            username = 'asdf',
+            password = 'qwerty'
+        ),
+        call().communicate()
+    ])
+    assert {'code': 1, 'message': "remote: Invalid username or password.\r\nfatal: Authentication failed for 'repo_url'"} == actual_response
 
 @patch('subprocess.Popen')
 @patch('os.environ', {'TEST': 'test'})
@@ -62,6 +94,37 @@ def test_git_pull_success(mock_subproc_popen):
     ])
     assert {'code': 0} == actual_response
 
+@patch('jupyterlab_git.git.git_auth_input_wrapper')
+@patch('os.environ', {'TEST': 'test'})
+def test_git_pull_with_auth_success(mock_git_auth_input_wrapper):
+    # Given
+    process_mock = Mock()
+    attrs = {
+        'communicate.return_value': ('output', ''.encode('utf-8')),
+        'returncode': 0
+    }
+    process_mock.configure_mock(**attrs)
+    mock_git_auth_input_wrapper.return_value = process_mock
+
+    # When
+    auth = {
+        'username' : 'asdf', 
+        'password' : 'qwerty'
+    }
+    actual_response = Git(root_dir='/bin').pull('test_curr_path', auth)
+
+    # Then
+    mock_git_auth_input_wrapper.assert_has_calls([
+        call(
+            command = 'git pull --no-commit',
+            cwd='/bin/test_curr_path',
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '1'},
+            username = 'asdf',
+            password = 'qwerty'
+        ),
+        call().communicate()
+    ])
+    assert {'code': 0} == actual_response
 
 @patch('subprocess.Popen')
 @patch('os.environ', {'TEST': 'test'})
@@ -91,6 +154,38 @@ def test_git_push_fail(mock_subproc_popen):
     ])
     assert {'code': 1, 'message': 'Authentication failed'} == actual_response
 
+@patch('jupyterlab_git.git.git_auth_input_wrapper')
+@patch('os.environ', {'TEST': 'test'})
+def test_git_push_with_auth_fail(mock_git_auth_input_wrapper):
+    # Given
+    process_mock = Mock()
+    attrs = {
+        'communicate.return_value': "remote: Invalid username or password.\r\nfatal: Authentication failed for 'repo_url'".encode('utf-8'),
+        'returncode': 1
+    }
+    process_mock.configure_mock(**attrs)
+    mock_git_auth_input_wrapper.return_value = process_mock
+
+    # When
+    auth = {
+        'username' : 'asdf', 
+        'password' : 'qwerty'
+    }
+    actual_response = Git(root_dir='/bin').push('test_origin', 'HEAD:test_master', 'test_curr_path', auth)
+
+    # Then
+    mock_git_auth_input_wrapper.assert_has_calls([
+        call(
+            command = 'git push test_origin HEAD:test_master',
+            cwd='/bin/test_curr_path',
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '1'},
+            username = 'asdf',
+            password = 'qwerty'
+        ),
+        call().communicate()
+    ])
+    assert {'code': 1, 'message': "remote: Invalid username or password.\r\nfatal: Authentication failed for 'repo_url'"} == actual_response
+
 
 @patch('subprocess.Popen')
 @patch('os.environ', {'TEST': 'test'})
@@ -115,6 +210,38 @@ def test_git_push_success(mock_subproc_popen):
             stderr=PIPE,
             cwd='/bin/test_curr_path',
             env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
+        ),
+        call().communicate()
+    ])
+    assert {'code': 0} == actual_response
+
+@patch('jupyterlab_git.git.git_auth_input_wrapper')
+@patch('os.environ', {'TEST': 'test'})
+def test_git_push_with_auth_success(mock_git_auth_input_wrapper):
+    # Given
+    process_mock = Mock()
+    attrs = {
+        'communicate.return_value': 'does not matter'.encode('utf-8'),
+        'returncode': 0
+    }
+    process_mock.configure_mock(**attrs)
+    mock_git_auth_input_wrapper.return_value = process_mock
+
+    # When
+    auth = {
+        'username' : 'asdf', 
+        'password' : 'qwerty'
+    }
+    actual_response = Git(root_dir='/bin').push('.', 'HEAD:test_master', 'test_curr_path', auth)
+
+    # Then
+    mock_git_auth_input_wrapper.assert_has_calls([
+        call(
+            command = 'git push . HEAD:test_master',
+            cwd='/bin/test_curr_path',
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '1'},
+            username = 'asdf',
+            password = 'qwerty'
         ),
         call().communicate()
     ])
