jupyterlab/jupyterlab-git

commit b3f3fbe4984e5b3f1f006936acf29f26a317a4ea

Fix/push pull windows (#344)
* fix/push-pull-windows: fixes #291; add GIT_TERMINAL_PROMPT in env arg

* fix/push-pull-windows: update push pull functions

* fix/push-pull-windows: update tests

* fix/push-pull-windows: update clone tests

* fix/push-pull-windows: fix tests to actually check env

* fix/push-pull-windows: remove unused import

* fix/push-pull-windows: remove temp variable

jupyterlab_git/git.py
@@ -23,12 +23,15 @@ class Git:
         :param repo_url: the URL of the repository to be cloned.
         :return: response with status code and error message.
         """
+        env = os.environ.copy()
+        env['GIT_TERMINAL_PROMPT'] = '0'
         p = subprocess.Popen(
-            ['GIT_TERMINAL_PROMPT=0 git clone {}'.format(unquote(repo_url))],
+            ['git', 'clone', unquote(repo_url)],
             shell=True,
             stdout=PIPE,
             stderr=PIPE,
             cwd=os.path.join(self.root_dir, current_path),
+            env=env,
         )
         _, error = p.communicate()
 
@@ -449,13 +452,15 @@ class Git:
         Execute git pull --no-commit.  Disables prompts for the password to avoid the terminal hanging while waiting
         for auth.
         """
-
+        env = os.environ.copy()
+        env['GIT_TERMINAL_PROMPT'] = '0'
         p = subprocess.Popen(
-            ['GIT_TERMINAL_PROMPT=0 git pull --no-commit'],
+            ['git', 'pull', '--no-commit'],
             shell=True,
             stdout=PIPE,
             stderr=PIPE,
             cwd=os.path.join(self.root_dir, curr_fb_path),
+            env=env,
         )
         _, error = p.communicate()
 
@@ -472,12 +477,15 @@ class Git:
         """
         Execute `git push $UPSTREAM $BRANCH`. The choice of upstream and branch is up to the caller.
         """
+        env = os.environ.copy()
+        env['GIT_TERMINAL_PROMPT'] = '0'
         p = subprocess.Popen(
-            ['GIT_TERMINAL_PROMPT=0 git push {} {}'.format(remote, branch)],
+            ['git', 'push', remote, branch],
             shell=True,
             stdout=PIPE,
             stderr=PIPE,
             cwd=os.path.join(self.root_dir, curr_fb_path),
+            env=env,
         )
         _, error = p.communicate()
 

tests/unit/test_clone.py
@@ -6,6 +6,7 @@ from jupyterlab_git.git import Git
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_clone_success(mock_subproc_popen):
     # Given
     process_mock = Mock()
@@ -22,11 +23,12 @@ def test_git_clone_success(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git clone ghjkhjkl'],
+            ['git', 'clone', 'ghjkhjkl'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])
@@ -34,6 +36,7 @@ def test_git_clone_success(mock_subproc_popen):
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_clone_failure_from_git(mock_subproc_popen):
     """
     Git internally will throw an error if it is an invalid URL, or if there is a permissions issue. We want to just
@@ -55,11 +58,12 @@ def test_git_clone_failure_from_git(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git clone ghjkhjkl'],
+            ['git', 'clone', 'ghjkhjkl'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])

tests/unit/test_pushpull.py
@@ -6,6 +6,7 @@ from jupyterlab_git.git import Git
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_pull_fail(mock_subproc_popen):
     # Given
     process_mock = Mock()
@@ -22,11 +23,12 @@ def test_git_pull_fail(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git pull --no-commit'],
+            ['git', 'pull', '--no-commit'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])
@@ -34,6 +36,7 @@ def test_git_pull_fail(mock_subproc_popen):
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_pull_success(mock_subproc_popen):
     # Given
     process_mock = Mock()
@@ -50,11 +53,12 @@ def test_git_pull_success(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git pull --no-commit'],
+            ['git', 'pull', '--no-commit'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])
@@ -62,6 +66,7 @@ def test_git_pull_success(mock_subproc_popen):
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_push_fail(mock_subproc_popen):
     # Given
     process_mock = Mock()
@@ -78,11 +83,12 @@ def test_git_push_fail(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git push test_origin HEAD:test_master'],
+            ['git', 'push', 'test_origin', 'HEAD:test_master'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])
@@ -90,6 +96,7 @@ def test_git_push_fail(mock_subproc_popen):
 
 
 @patch('subprocess.Popen')
+@patch('os.environ', {'TEST': 'test'})
 def test_git_push_success(mock_subproc_popen):
     # Given
     process_mock = Mock()
@@ -106,11 +113,12 @@ def test_git_push_success(mock_subproc_popen):
     # Then
     mock_subproc_popen.assert_has_calls([
         call(
-            ['GIT_TERMINAL_PROMPT=0 git push . HEAD:test_master'],
+            ['git', 'push', '.', 'HEAD:test_master'],
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True
+            shell=True,
+            env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
     ])

commit c08bdaa4ce848aa56bb860591f678c8752407a25

Update clone to not use shell=True
Related to
* Similar fix made for push/pull https://github.com/jupyterlab/jupyterlab-git/pull/362
* Regression from https://github.com/jupyterlab/jupyterlab-git/pull/344

jupyterlab_git/git.py
@@ -78,7 +78,6 @@ class Git:
         env['GIT_TERMINAL_PROMPT'] = '0'
         p = subprocess.Popen(
             ['git', 'clone', unquote(repo_url)],
-            shell=True,
             stdout=PIPE,
             stderr=PIPE,
             cwd=os.path.join(self.root_dir, current_path),

tests/unit/test_clone.py
@@ -27,7 +27,6 @@ def test_git_clone_success(mock_subproc_popen):
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True,
             env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()
@@ -62,7 +61,6 @@ def test_git_clone_failure_from_git(mock_subproc_popen):
             stdout=PIPE,
             stderr=PIPE,
             cwd='/bin/test_curr_path',
-            shell=True,
             env={'TEST': 'test', 'GIT_TERMINAL_PROMPT': '0'},
         ),
         call().communicate()