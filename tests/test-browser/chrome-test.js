2b8d8992605d05b69c23f9359db8e44227e0a849
Frederic Collonval <frederic.collonval@safrangroup.com>
2019-07-30 03:01:51 -0400

    fix browser_check

--- tests/test-browser/chrome-test.js
+++ tests/test-browser/chrome-test.js
@@ -1,44 +1,19 @@
 const assert = require('assert');
 const puppeteer = require('puppeteer');
 const inspect = require('util').inspect;
-
-function getUrl() {
-  return process.argv[2];
-}
-
-function testJupyterLabPage(html) {
-  if (inspect(html).indexOf('jupyter-config-data') === -1) {
-    console.error('Error loading JupyterLab page:');
-    console.error(html);
-  }
-}
-
-async function testApplication(page) {
-  const el = await page.waitForSelector('#browserTest', { timeout: 100000 });
-  console.log('Waiting for application to start...');
-
-  await page.waitForSelector('.completed');
-
-  const textContent = await el.getProperty('textContent');
-  const errors = JSON.parse(await textContent.jsonValue());
-
-  for (let error of errors) {
-    console.error(`Parsed an error from text content: ${error.message}`, error);
-    throw error;
-  }
-}
+const URL = process.argv[2];
 
 function testGitExtension(html) {
-    // Test git icons are present
-    assert(html.includes('--jp-icon-git-clone'), 'Could not find git clone icon.');
-    assert(html.includes('--jp-icon-git-pull'), 'Could not find git pull icon.');
-    assert(html.includes('--jp-icon-git-push'), 'Could not find git push icon.');
+  // Test git icons are present
+  assert(html.includes('--jp-icon-git-clone'), 'Could not find git clone icon.');
+  assert(html.includes('--jp-icon-git-pull'), 'Could not find git pull icon.');
+  assert(html.includes('--jp-icon-git-push'), 'Could not find git push icon.');
 }
 
 async function main() {
+  /* eslint-disable no-console */
   console.info('Starting Chrome Headless');
 
-  const URL = getUrl();
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
@@ -46,13 +21,38 @@ async function main() {
   await page.goto(URL);
   console.info('Waiting for page to load...');
 
-  const html = await page.content();
-  testJupyterLabPage(html);
+  // Wait for the local file to redirect on notebook >= 6.0
+  await page.waitForNavigation();
 
-  await testApplication(page);
+  const html = await page.content();
+  if (inspect(html).indexOf('jupyter-config-data') === -1) {
+    console.error('Error loading JupyterLab page:');
+    console.error(html);
+  }
+
+  const el = await page.waitForSelector('#browserTest', { timeout: 100000 });
+  console.log('Waiting for application to start...');
+  let testError = null;
+
+  try {
+    await page.waitForSelector('.completed');
+  } catch (e) {
+    testError = e;
+  }
+  const textContent = await el.getProperty('textContent');
+  const errors = JSON.parse(await textContent.jsonValue());
+
+  for (let error of errors) {
+    console.error(`Parsed an error from text content: ${error.message}`, error);
+  }
+
   testGitExtension(html);
 
   await browser.close();
+
+  if (testError) {
+    throw testError;
+  }
   console.info('Chrome test complete');
 }
 
