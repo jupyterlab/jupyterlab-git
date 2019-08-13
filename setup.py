bcbe348b95ca493bc29cf9bf3f50b84e04c32dc6
Frédéric Collonval <fcollonval@gmail.com>
2019-08-13 13:38:01 +0200

    Minimal jlab version for testing

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        'jupyterlab >= 1.0.0',
+        'jupyterlab >= 1.0.3',
         'nbdime >= 1.1.0'
     ],
     package_data={'jupyterlab_git': ['*']},
