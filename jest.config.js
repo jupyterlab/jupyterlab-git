665e47153a0f2889498b5f2881107b275fb61f20
Frederic Collonval <frederic.collonval@safrangroup.com>
2019-10-17 13:43:43 -0400

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   automock: false,
   moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(svg)$': '<rootDir>/testutils/jest-file-mock.js'
   },
   preset: 'ts-jest/presets/js-with-babel',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./setupJest.js'],
+  setupFiles: ['<rootDir>/testutils/jest-setup-files.js'],
   testPathIgnorePatterns: ['/dev_mode/', '/lib/', '/node_modules/'],
   testRegex: '/tests/test-.*/.*.spec.ts[x]?$',
   transformIgnorePatterns: ['/node_modules/(?!(@jupyterlab/.*)/)'],
