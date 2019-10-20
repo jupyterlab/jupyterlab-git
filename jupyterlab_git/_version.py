c6a4319c6dcbce5a02f56bd3a59202d7e7b2c5a7
telamonian <mklein@jhu.edu>
2019-10-20 05:23:51 -0400

    bumped python package version to match labextension

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 8, 2)
+version_info = (0, 9, 0)
 __version__ = ".".join(map(str, version_info))
