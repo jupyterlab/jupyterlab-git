c7e6909446904f0ed4347a6f492a36b133d33847
telamonian <mklein@jhu.edu>
2019-10-20 08:59:29 -0400

    post rebase cleanup

--- src/components/GitPanel.tsx
+++ src/components/GitPanel.tsx
@@ -85,12 +85,6 @@ export class GitPanel extends React.Component<
       untrackedFiles: [],
       sideBarExpanded: false
     };
-
-    this.props.settings.changed.connect(this.refresh, this);
-  }
-
-  componentDidMount(): void {
-    this.refresh();
   }
 
   setShowList = (state: boolean) => {
