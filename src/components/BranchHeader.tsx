cf8a7095675cac962e2b96e66059c277d7587af2
Athan Reines <kgryte@gmail.com>
2020-01-04 20:09:20 -0800

    Remove obsolete component and logic

--- src/components/BranchHeader.tsx
+++ src/components/BranchHeader.tsx
@@ -2,30 +2,19 @@ import { showErrorMessage } from '@jupyterlab/apputils';
 import * as React from 'react';
 import { classes } from 'typestyle';
 import {
-  branchDropdownButtonStyle,
   branchHeaderCenterContent,
-  branchLabelStyle,
-  branchListItemStyle,
   branchStyle,
-  branchTrackingLabelStyle,
-  expandedBranchStyle,
-  headerButtonDisabledStyle,
   historyLabelStyle,
-  newBranchButtonStyle,
   openHistorySideBarButtonStyle,
   selectedHeaderStyle,
   unSelectedHeaderStyle
 } from '../style/BranchHeaderStyle';
 import { Git, IGitExtension } from '../tokens';
-import { NewBranchBox } from './NewBranchBox';
 
 const CHANGES_ERR_MSG =
   'You have files with changes in current branch. Please commit or discard changed files before';
 
-export interface IBranchHeaderState {
-  dropdownOpen: boolean;
-  showNewBranchBox: boolean;
-}
+export interface IBranchHeaderState {}
 
 export interface IBranchHeaderProps {
   model: IGitExtension;
@@ -45,40 +34,15 @@ export class BranchHeader extends React.Component<
 > {
   constructor(props: IBranchHeaderProps) {
     super(props);
-    this.state = {
-      dropdownOpen: false,
-      showNewBranchBox: false
-    };
+    this.state = {};
   }
-
-  /** Switch current working branch */
-  async switchBranch(branchName: string) {
-    const result = await this.props.model.checkout({ branchname: branchName });
-    if (result.code !== 0) {
-      showErrorMessage('Error switching branch', result.message);
-    }
-
-    this.toggleSelect();
-  }
-
-  createNewBranch = async (branchName: string) => {
-    const result = await this.props.model.checkout({
-      newBranch: true,
-      branchname: branchName
-    });
-    if (result.code !== 0) {
-      showErrorMessage('Error creating new branch', result.message);
-    }
-
-    this.toggleNewBranchBox();
-  };
 
   toggleSelect() {
     this.props.refresh();
     if (!this.props.disabled) {
-      this.setState({
-        dropdownOpen: !this.state.dropdownOpen
-      });
+      // this.setState({
+      //   dropdownOpen: !this.state.dropdownOpen
+      // });
     } else {
       showErrorMessage(
         'Switching branch disabled',
@@ -86,29 +50,6 @@ export class BranchHeader extends React.Component<
       );
     }
   }
-
-  getBranchStyle() {
-    if (this.state.dropdownOpen) {
-      return classes(branchStyle, expandedBranchStyle);
-    } else {
-      return branchStyle;
-    }
-  }
-
-  toggleNewBranchBox = (): void => {
-    this.props.refresh();
-    if (!this.props.disabled) {
-      this.setState({
-        showNewBranchBox: !this.state.showNewBranchBox,
-        dropdownOpen: false
-      });
-    } else {
-      showErrorMessage(
-        'Creating new branch disabled',
-        CHANGES_ERR_MSG + 'creating a new branch.'
-      );
-    }
-  };
 
   getHistoryHeaderStyle() {
     if (this.props.sideBarExpanded) {
@@ -126,7 +67,7 @@ export class BranchHeader extends React.Component<
 
   render() {
     return (
-      <div className={this.getBranchStyle()}>
+      <div className={branchStyle}>
         <div style={{ display: 'flex' }}>
           <div
             className={this.getHistoryHeaderStyle()}
@@ -147,66 +88,9 @@ export class BranchHeader extends React.Component<
                 : null
             }
           >
-            <h3 className={branchLabelStyle}>{this.props.currentBranch}</h3>
-            <div
-              className={
-                this.props.disabled
-                  ? classes(
-                      branchDropdownButtonStyle,
-                      headerButtonDisabledStyle
-                    )
-                  : branchDropdownButtonStyle
-              }
-              title={'Change the current branch'}
-              onClick={() => this.toggleSelect()}
-            />
-            {!this.state.showNewBranchBox && (
-              <div
-                className={
-                  this.props.disabled
-                    ? classes(newBranchButtonStyle, headerButtonDisabledStyle)
-                    : newBranchButtonStyle
-                }
-                title={'Create a new branch'}
-                onClick={() => this.toggleNewBranchBox()}
-              />
-            )}
-            {this.state.showNewBranchBox && (
-              <NewBranchBox
-                createNewBranch={this.createNewBranch}
-                toggleNewBranchBox={this.toggleNewBranchBox}
-              />
-            )}
-            {this.props.upstreamBranch != null &&
-              this.props.upstreamBranch !== '' && (
-                <h3 className={branchTrackingLabelStyle}>
-                  {this.props.upstreamBranch}
-                </h3>
-              )}
+            <h3 className={historyLabelStyle}>Changes</h3>
           </div>
         </div>
-        {!this.props.sideBarExpanded && (
-          <React.Fragment>
-            {this.state.dropdownOpen && (
-              <div>
-                {this.props.data.map(
-                  (branch: Git.IBranch, branchIndex: number) => (
-                    <li
-                      className={branchListItemStyle}
-                      key={branchIndex}
-                      onClick={() => this.switchBranch(branch.name)}
-                    >
-                      {branch.name}
-                    </li>
-                  )
-                )}
-              </div>
-            )}
-            {this.state.showNewBranchBox && (
-              <div>Branching from {this.props.currentBranch}</div>
-            )}
-          </React.Fragment>
-        )}
       </div>
     );
   }
