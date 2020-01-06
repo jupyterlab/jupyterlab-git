13a53be0d1eca512193c455dbd6349e35084bdde
Athan Reines <kgryte@gmail.com>
2020-01-06 11:02:42 -0800

    Adjust tab styles and remove unused classes

--- src/style/BranchHeaderStyle.ts
+++ src/style/BranchHeaderStyle.ts
@@ -18,12 +18,8 @@ export const unSelectedHeaderStyle = style({
   paddingTop: 'var(--jp-border-width)'
 });
 
-export const expandedBranchStyle = style({
-  height: '500px'
-});
-
 export const openHistorySideBarButtonStyle = style({
-  width: '50px',
+  width: '50%',
   flex: 'initial',
   paddingLeft: '10px',
   paddingRight: '10px',
@@ -38,57 +34,9 @@ export const historyLabelStyle = style({
   fontWeight: 'normal'
 });
 
-export const branchLabelStyle = style({
-  fontSize: 'var(--jp-ui-font-size1)',
-  marginTop: '5px',
-  marginBottom: '5px',
-  display: 'inline-block'
-});
-
-export const branchTrackingLabelStyle = style({
-  fontSize: 'var(--jp-ui-font-size1)',
-  marginTop: '5px',
-  marginBottom: '5px',
-  display: 'inline-block',
-  color: 'var(--jp-ui-font-color2)',
-  fontWeight: 'normal'
-});
-
-export const branchDropdownButtonStyle = style({
-  backgroundImage: 'var(--jp-icon-arrow-down)',
-  backgroundSize: '100%',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  height: '18px',
-  width: '18px',
-  display: 'inline-block',
-  verticalAlign: 'middle'
-});
-
-export const newBranchButtonStyle = style({
-  backgroundImage: 'var(--jp-icon-plus)',
-  backgroundSize: '100%',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  height: '18px',
-  width: '18px',
-  display: 'inline-block',
-  verticalAlign: 'middle'
-});
-
-export const headerButtonDisabledStyle = style({
-  opacity: 0.5
-});
-
-export const branchListItemStyle = style({
-  listStyle: 'none',
-  textAlign: 'left',
-  marginLeft: '1em',
-  color: 'var(--jp-ui-font-color1)'
-});
-
 export const branchHeaderCenterContent = style({
-  paddingLeft: '5px',
-  paddingRight: '5px',
-  flex: 'auto'
+  paddingLeft: '10px',
+  paddingRight: '10px',
+  flex: 'auto',
+  width: '50%'
 });
