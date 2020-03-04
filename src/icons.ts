9bf681e7c0001d1c831b8e0af45f0dcb53604b61
telamonian <mklein@jhu.edu>
2020-03-03 21:32:05 -0500

    fixed some icon paths/names

--- src/icons.ts
+++ src/icons.ts
@@ -1,16 +1,10 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
 // icon svg import statements
-import gitSvgstr from '../../style/images/git-icon.svg';
-import deletionsSvgstr from '../../style/images/deletions-made-icon.svg';
-import insertionsSvgstr from '../../style/images/insertions-made-icon.svg';
+import gitSvgstr from '../style/images/git.svg';
+import deletionsSvgstr from '../style/images/deletions.svg';
+import insertionsSvgstr from '../style/images/insertions.svg';
 
 export const gitIcon = new LabIcon({ name: 'git:git', svgstr: gitSvgstr });
-export const deletionsIcon = new LabIcon({
-  name: 'git:deletions',
-  svgstr: deletionsSvgstr
-});
-export const insertionsIcon = new LabIcon({
-  name: 'git:insertions',
-  svgstr: insertionsSvgstr
-});
+export const deletionsIcon = new LabIcon({ name: 'git:deletions', svgstr: deletionsSvgstr });
+export const insertionsIcon = new LabIcon({ name: 'git:insertions', svgstr: insertionsSvgstr });
