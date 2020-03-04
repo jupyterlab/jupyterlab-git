40c80435fcc5b279b813da1598a491eee68915ac
telamonian <mklein@jhu.edu>
2020-03-04 00:54:41 -0500

    moved style/images => style/icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,9 +1,9 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
 // icon svg import statements
-import gitSvgstr from '../style/images/git.svg';
-import deletionsSvgstr from '../style/images/deletions.svg';
-import insertionsSvgstr from '../style/images/insertions.svg';
+import gitSvgstr from '../style/icons/git.svg';
+import deletionsSvgstr from '../style/icons/deletions.svg';
+import insertionsSvgstr from '../style/icons/insertions.svg';
 
 export const gitIcon = new LabIcon({ name: 'git:git', svgstr: gitSvgstr });
 export const deletionsIcon = new LabIcon({ name: 'git:deletions', svgstr: deletionsSvgstr });
