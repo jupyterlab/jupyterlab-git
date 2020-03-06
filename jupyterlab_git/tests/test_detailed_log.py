98d06a2bfb8e538b0efdbe0ca5300b54fe22bef1
ianhi <ianhuntisaak@gmail.com>
2020-03-06 16:51:08 -0500

    Add λ test case
    detailed log takes a weird mix of \x00 and \n as line separators hence the odd joining.

--- jupyterlab_git/tests/test_detailed_log.py
+++ jupyterlab_git/tests/test_detailed_log.py
@@ -15,28 +15,35 @@ from .testutils import FakeContentManager
 async def test_detailed_log():
     with patch("jupyterlab_git.git.execute") as mock_execute:
         # Given
-        process_output = [
+        process_output_first_half = [
             "f29660a (HEAD, origin/feature) Commit message",
             "10\t3\tnotebook_without_spaces.ipynb",
             "11\t4\tNotebook with spaces.ipynb",
             "12\t5\tpath/notebook_without_spaces.ipynb",
             "13\t6\tpath/Notebook with spaces.ipynb",
+            "14\t1\tpath/Notebook with λ.ipynb",
+        ]
+        process_output_second_half = [
             " notebook_without_spaces.ipynb      | 13 ++++++++---",
             " Notebook with spaces.ipynb         | 15 +++++++++----",
             " path/notebook_without_spaces.ipynb | 17 ++++++++++-----",
             " path/Notebook with spaces.ipynb    | 19 +++++++++++------",
-            " 4 files changed, 46 insertions(+), 18 deletions(-)",
+            " path/Notebook with λ.ipynb         | 15 +++++++++++-",
+            " 5 files changed, 50 insertions(+), 19 deletions(-)",
         ]
-        mock_execute.return_value = tornado.gen.maybe_future(
-            (0, "\x00".join(process_output), "")
+        process_output_first_half = "\x00".join(process_output_first_half)
+        process_output_second_half = "\n".join(process_output_second_half)
+        process_output = process_output_first_half + "\x00" + process_output_second_half
+        mock_execute._mock_return_value = tornado.gen.maybe_future(
+            (0, process_output, "")
         )
 
         expected_response = {
             "code": 0,
-            "modified_file_note": " 4 files changed, 46 insertions(+), 18 deletions(-)",
-            "modified_files_count": "4",
-            "number_of_insertions": "46",
-            "number_of_deletions": "18",
+            "modified_file_note": " 5 files changed, 50 insertions(+), 19 deletions(-)",
+            "modified_files_count": "5",
+            "number_of_insertions": "50",
+            "number_of_deletions": "19",
             "modified_files": [
                 {
                     "modified_file_path": "notebook_without_spaces.ipynb",
@@ -61,6 +68,12 @@ async def test_detailed_log():
                     "modified_file_name": "Notebook with spaces.ipynb",
                     "insertion": "13",
                     "deletion": "6",
+                },
+                {
+                    "modified_file_path": "path/Notebook with λ.ipynb",
+                    "modified_file_name": "Notebook with λ.ipynb",
+                    "insertion": "14",
+                    "deletion": "1",
                 },
             ],
         }
