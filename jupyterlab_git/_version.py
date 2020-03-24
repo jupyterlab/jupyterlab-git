4ec602addf35e19b5bde40b1134e505ff0fc94f5
Steven Silvester <stslve@amazon.com>
2020-03-24 11:26:01 -0500

    Release 0.9.1

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 9, 0)
+version_info = (0, 9, 1)
 __version__ = ".".join(map(str, version_info))
