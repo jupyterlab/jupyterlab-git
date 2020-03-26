557cb06bdc1dc8894443a78475a023f2c38c315b
telamonian <telamonian@users.noreply.github.com>
2020-03-25 22:13:07 -0400

    bump master to v0.20.0

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 10, 0)
+version_info = (0, 20, 0)
 flag = ''
 
 __version__ = ".".join(map(str, version_info)) + flag
