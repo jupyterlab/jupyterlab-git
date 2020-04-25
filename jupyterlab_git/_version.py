903abd627c8244d05329be7703b3ded9e0269be8
Frederic Collonval <fcollonval@gmail.com>
2020-04-25 14:30:11 +0200

    Bump to 0.10.1rc0

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 10, 0)
-flag = ''
+version_info = (0, 10, 1)
+flag = 'rc0'
 
 __version__ = ".".join(map(str, version_info)) + flag
