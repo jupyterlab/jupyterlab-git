9ff96d5ea49f1b0f269a9b11a77b6410c62022ff
Jaipreet Singh <jaipreet@amazon.com>
2020-07-23 19:11:56 -0700

    Fix tests and increment patch version

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 10, 1)
-flag = ''
+version_info = (0, 10, 2)
+flag = 'patch'
 
 __version__ = ".".join(map(str, version_info)) + flag
