7238a9232821c17744325d0d9478546895bbc9cb
Frédéric Collonval <fcollonval@gmail.com>
2020-08-30 13:07:06 +0200

    Backport PR #748: Theme status widget text

--- src/style/StatusWidget.ts
+++ src/style/StatusWidget.ts
@@ -1,5 +1,8 @@
 import { style } from 'typestyle';
 
 export const statusWidgetClass = style({
+  color: 'var(--jp-ui-font-color1)',
+  fontFamily: 'var(--jp-ui-font-family)',
+  fontSize: 'var(--jp-ui-font-size1)',
   lineHeight: '24px'
 });
