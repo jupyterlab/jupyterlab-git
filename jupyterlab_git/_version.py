c5ce0a38faba1f69a5c0315ab69c6272bf308594
Frédéric Collonval <fcollonval@gmail.com>
2020-12-24 11:34:55 +0100

    Bump to v0.23.3 (#850)

--- jupyterlab_git/_version.py
+++ jupyterlab_git/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 23, 2)
+version_info = (0, 23, 3)
 flag = ""
 
 __version__ = ".".join(map(str, version_info)) + flag
