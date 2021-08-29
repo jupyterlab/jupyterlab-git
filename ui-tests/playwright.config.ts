fd8b439afb4dd4ae722ce2c9d1cc81c190377985
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2021-08-29 11:33:27 -0400

    Add merge conflict example repo archive

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -4,14 +4,14 @@ const config: PlaywrightTestConfig = {
   timeout: 60 * 1000,
   use: {
     // Browser options
-    // headless: false,
+    headless: true,
 
     // Context options
     viewport: { width: 1280, height: 720 },
 
     // Artifacts
-    video: 'on',
-  },
+    video: 'on'
+  }
 };
 
 export default config;
