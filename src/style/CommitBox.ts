a8e7255d58332fb5b30f0a4ea1f0ad1bfafa0a60
Andrew H. Li <andrew@software.com>
2021-09-15 07:17:46 -0500

    Fix pointer on CommitBox not displaying
    PR request based on @fcollonval's suggestion from this thread: https://github.com/jupyterlab/jupyterlab-git/issues/1035#issuecomment-919755588

--- src/style/CommitBox.ts
+++ src/style/CommitBox.ts
@@ -109,5 +109,6 @@ export const activeStyle = style({
 export const disabledStyle = style({
   cursor: 'not-allowed !important',
   color: 'var(--jp-ui-font-color2) !important',
-  backgroundColor: 'var(--jp-layout-color3) !important'
+  backgroundColor: 'var(--jp-layout-color3) !important',
+  pointerEvents: 'auto !important'
 });
