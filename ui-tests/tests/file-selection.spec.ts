48b38cab3400b31f91c47ba444231896e2c7ad3b
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-07-07 12:18:08 -0700

    added simple ui tests for ctlr and shift click on file selection for normal staging

---
+++ ui-tests/tests/file-selection.spec.ts
@@ -0,0 +1,52 @@
+import { test } from '@jupyterlab/galata';
+import { expect } from '@playwright/test';
+import path from 'path';
+import { extractFile } from './utils';
+
+const baseRepositoryPath = 'test-repository.tar.gz';
+test.use({ autoGoto: false });
+
+test.describe('File selection for normal staging', () => {
+  test.beforeEach(async ({ baseURL, page, tmpPath }) => {
+    await extractFile(
+      baseURL,
+      path.resolve(__dirname, 'data', baseRepositoryPath),
+      path.join(tmpPath, 'repository.tar.gz')
+    );
+
+    // URL for merge conflict example repository
+    await page.goto(`tree/${tmpPath}/repository`);
+  });
+
+  test('should select two files with ctlr-click', async ({ page }) => {
+    await page.sidebar.openTab('jp-git-sessions');
+    await page.click('button:has-text("Changes")');
+    page.pause();
+
+    // Click another_file.txt
+    await page.locator('#jp-git-sessions >> text=another_file.txt').click();
+    // Control-click master_file.ts
+    await page.locator('#jp-git-sessions >> text=master_file.ts').click({
+      modifiers: ['Control', 'Meta']
+    });
+    const selectedFiles = page.locator('.f3z92wx');
+    expect(await selectedFiles.count()).toBeGreaterThanOrEqual(2);
+
+  });
+
+  test('should select four files with shift-click', async ({ page }) => {
+    await page.sidebar.openTab('jp-git-sessions');
+    await page.click('button:has-text("Changes")');
+    page.pause();
+
+    // Click another_file.txt
+    await page.locator('#jp-git-sessions >> text=another_file.txt').click();
+    // Shift-click master_file.ts
+    await page.locator('#jp-git-sessions >> text=master_file.ts').click({
+      modifiers: ['Shift']
+    });
+
+    const selectedFiles = page.locator('.f3z92wx');
+    expect(await selectedFiles.count()).toBeGreaterThanOrEqual(4);
+  });
+});
