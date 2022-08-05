9c686a42352ec012b6e3209404d6fce674ecd0ab
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-08-05 13:18:36 -0700

    Remove test for git:add-remote command

--- tests/commands.spec.tsx
+++ tests/commands.spec.tsx
@@ -58,61 +58,6 @@ describe('git-commands', () => {
     );
   });
 
-  // describe('git:manage-remote', () => {
-  //   it('should admit user and name arguments', async () => {
-  //     const name = 'ref';
-  //     const url = 'https://www.mygitserver.com/me/myrepo.git';
-  //     const path = DEFAULT_REPOSITORY_PATH;
-
-  //     mockGit.requestAPI.mockImplementation(
-  //       mockedRequestAPI({
-  //         ...mockResponses,
-  //         'remote/add': {
-  //           body: () => {
-  //             return { code: 0, command: `git remote add ${name} ${url}` };
-  //           }
-  //         }
-  //       })
-  //     );
-
-  //     model.pathRepository = path;
-  //     await model.ready;
-
-  //     await commands.execute(CommandIDs.gitManageRemote, { url, name });
-
-  //     expect(mockGit.requestAPI).toBeCalledWith(`${path}/remote/add`, 'POST', {
-  //       url,
-  //       name
-  //     });
-  //   });
-
-  //   it('has optional argument name', async () => {
-  //     const name = 'origin';
-  //     const url = 'https://www.mygitserver.com/me/myrepo.git';
-  //     const path = DEFAULT_REPOSITORY_PATH;
-
-  //     mockGit.requestAPI.mockImplementation(
-  //       mockedRequestAPI({
-  //         ...mockResponses,
-  //         'remote/add': {
-  //           body: () => {
-  //             return { code: 0, command: `git remote add ${name} ${url}` };
-  //           }
-  //         }
-  //       })
-  //     );
-
-  //     model.pathRepository = path;
-  //     await model.ready;
-
-  //     await commands.execute(CommandIDs.gitManageRemote);
-
-  //     expect(mockGit.requestAPI).toBeCalledWith(`${path}/remote/add`, 'POST', {
-  //       url
-  //     });
-  //   });
-  // });
-
   describe('git:context-discard', () => {
     ['staged', 'partially-staged', 'unstaged', 'untracked'].forEach(status => {
       [' ', 'M', 'A'].forEach(x => {
