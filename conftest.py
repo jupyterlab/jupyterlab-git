1a3ca6edbe6aa4d49f83f816d859b7d1206ea8d8
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2022-09-06 13:21:37 +0530

    Add test

--- conftest.py
+++ conftest.py
@@ -4,5 +4,8 @@ pytest_plugins = ["jupyter_server.pytest_plugin"]
 
 
 @pytest.fixture
-def jp_server_config(jp_server_config):
-    return {"ServerApp": {"jpserver_extensions": {"jupyterlab_git": True}}}
+def jp_server_config(jp_server_config, jp_root_dir):
+    return {
+        "ServerApp": {"jpserver_extensions": {"jupyterlab_git": True}},
+        "JupyterLabGit": {"excluded_paths": [f"/test-path/*"]},
+    }
