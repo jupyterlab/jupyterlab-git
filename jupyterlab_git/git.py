1aee67509b1f43e7969ed9a2a39057a49f760787
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2022-10-27 10:59:22 -0400

    fix build failures

--- jupyterlab_git/git.py
+++ jupyterlab_git/git.py
@@ -286,14 +286,14 @@ class Git:
         cmd = ["git", "clone"]
         if versioning:
             cmd.append("--depth=1")
+            cmd.append("-q")
+            current_content = set(os.listdir(path))
         cmd.append(unquote(repo_url))
 
         if auth:
             if auth.get("cache_credentials"):
                 await self.ensure_credential_helper(path)
             env["GIT_TERMINAL_PROMPT"] = "1"
-            cmd.append("-q")
-            current_content = set(os.listdir(path))
             code, output, error = await execute(
                 cmd,
                 username=auth["username"],
