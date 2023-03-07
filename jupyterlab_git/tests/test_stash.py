2373b81ea5c3dd1540a7f4c6c6ab3b449c6d8a50
Shawn Esquivel <shawnesquivel24@gmail.com>
2023-03-06 19:00:26 -0800

    Added git stash failure test

--- jupyterlab_git/tests/test_stash.py
+++ jupyterlab_git/tests/test_stash.py
@@ -60,6 +60,43 @@ async def test_git_stash(mock_execute, jp_fetch, jp_root_dir):
     }
 
 
+@patch("jupyterlab_git.git.execute")
+async def test_git_stash_failure(mock_execute, jp_fetch, jp_root_dir):
+    # Given
+    # Creates a temporary directory created by pytest's tmp_path fixture and concatenates the string "test_path" to it.
+    env = os.environ.copy()
+    env["GIT_TERMINAL_PROMPT"] = "0"
+
+    local_path = jp_root_dir / "test_path"
+    error_msg = "Fake failure"
+    error_code = 128
+
+    # 0 = success code, "" = no output, "" = no error
+    mock_execute.return_value = maybe_future((error_code, "", error_msg))
+    # When
+    #
+
+    # jp_fetch is a fixture that returns a function that can be used to make HTTP requests to the Jupyter server.
+    """
+    jp_fetch(
+        NAMESPACE, # NAMESPACE is the prefix of the url
+        local_path.name, # local_path.name is the name of the folder
+        "stash", # stash is the name of the handler
+        body, empty because git stash doesn't take any arguments
+        method="POST", # method is the http method
+    )
+    """
+    with pytest.raises(tornado.httpclient.HTTPClientError) as e:
+        response = await jp_fetch(
+            NAMESPACE, local_path.name, "stash", body=json.dumps({}), method="POST"
+        )
+    assert_http_error(e, 500)
+
+    # Then
+    command = ["git", "stash"]
+    mock_execute.assert_called_once_with(command, cwd=str(local_path), env=env)
+
+
 # git stash when there are no changes
 
 # git stash when there are changes
