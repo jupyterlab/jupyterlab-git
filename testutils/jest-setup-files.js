6045e7e302fc7651a68c8cf56ecbb0b2486d8a72
Frédéric Collonval <fcollonval@gmail.com>
2023-04-05 11:33:38 +0200

    Fake crypto using node:crypto

--- testutils/jest-setup-files.js
+++ testutils/jest-setup-files.js
@@ -1,4 +1,7 @@
-global.fetch = require('jest-fetch-mock');
+globalThis.fetch = require('jest-fetch-mock');
 require("enzyme").configure({
     adapter: new (require('@wojtekmaj/enzyme-adapter-react-17'))
 });
+
+// Use node crypto for crypto
+globalThis.crypto = require('node:crypto');
