8d0f10a8bce8bc027786ad801d860484151228aa
Frédéric Collonval <fcollonval@gmail.com>
2023-04-05 12:23:14 +0200

    Fix node crypto import

--- testutils/jest-setup-files.js
+++ testutils/jest-setup-files.js
@@ -1,7 +1,7 @@
 globalThis.fetch = require('jest-fetch-mock');
+// Use node crypto for crypto
+globalThis.crypto = require('crypto');
+
 require("enzyme").configure({
     adapter: new (require('@wojtekmaj/enzyme-adapter-react-17'))
 });
-
-// Use node crypto for crypto
-globalThis.crypto = require('node:crypto');
