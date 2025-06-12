7cbe4dda5983b0b2f9435d9603606ba843dde33f
Greg Mooney <gregory.mooney@quantstack.net>
2025-06-12 10:35:11 +0200

    Use snake case for consistency
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>

--- jupyterlab_git/handlers.py
+++ jupyterlab_git/handlers.py
@@ -517,12 +517,12 @@ class GitCheckoutHandler(GitHandler):
         if data["checkout_branch"]:
             if data["new_check"]:
                 #  Need to check if startpoint is valid
-                isStartPointValid = await self.git._get_branch_reference(
+                is_start_point_valid = await self.git._get_branch_reference(
                     data["startpoint"], local_path
                 )
 
                 #  If start point is not valid, we need to make an empty commit
-                if not isStartPointValid:
+                if not is_start_point_valid:
                     await self.git._empty_commit(local_path)
 
                 body = await self.git.checkout_new_branch(
